365944851b5b192ddfd5fabe85267e7329d89931
seungrokoh <devdavidoh@gamil.com>
2018-07-20 17:35:40 +0900

    #10800 1차수정 ... 시간초과

--- #10800/10800.kt
+++ #10800/10800.kt
@@ -6,6 +6,8 @@ fun main(args: Array<String>) {
     val numOfBall = sc.nextInt()
     val color = arrayOfNulls<Int>(numOfBall)
     val ballSize = arrayOfNulls<Int>(numOfBall)
+    var score = IntArray(numOfBall, {0})
+
     var sum: Int = 0
     //init
     for (i in 0..numOfBall - 1) {
@@ -13,16 +15,21 @@ fun main(args: Array<String>) {
         ballSize.set(i, sc.nextInt())
     }
 
-
-    for (i in 0..numOfBall - 1) {
-        sum = 0
-        for (j in 0..numOfBall - 1) {
+    for (i in 0..numOfBall - 2) {
+        for (j in (i + 1)..numOfBall - 1) {
             if ((color.get(i)!! != color.get(j)!!)) {
-                if((ballSize.get(i)!! > ballSize.get(j)!!)){
-                    sum += ballSize.get(j)!!
+                //색상이 다를 때
+                if (ballSize.get(i)!! > ballSize.get(j)!!) {
+                    //나보다 작을 때
+                    score.set(i, score.get(i)!! + ballSize.get(j)!!)
+                }else {
+                    //나보다 클 때
+                    score.set(j, score.get(j)!! + ballSize.get(i)!!)
                 }
             }
         }
-        println(sum)
+    }
+    for (i in 0..numOfBall - 1) {
+        println(score.get(i))
     }
 }
