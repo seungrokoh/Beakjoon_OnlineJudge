c208972c724c8934a1e0fdb48e135450a7d8ae9a
seungrokoh <devdavidoh@gmail.com>
2018-07-24 17:36:41 +0900

    #15894 풀이완료

---
+++ #15894/15894.kt
@@ -0,0 +1,8 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc: Scanner = Scanner(System.`in`)
+    val num = sc.nextLong()
+    val result = num + num*1 + num*2
+    print(result)
+}
