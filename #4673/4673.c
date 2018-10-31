a33e239af50315832d6ddd031ea286eb2bf1a726
seungrokoh <devdavidoh@gmail.com>
2018-10-31 23:41:09 +0900

    #4673 풀이 수정1

--- #4673/4673.c
+++ #4673/4673.c
@@ -1,34 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int dn[10001];
+int arr[10001];
+
+int dn(int n){
+	int sum = n;
+	while(1){
+		if(n == 0){
+			break;	
+		}
+		sum += n%10;
+		n /= 10;
+	}
+	return sum;
+}
 
 int main(void){
-
-    int n = 10000;
-    int i, a,b,c,d,e = 0;
+    int i, result;
 
     for (i = 1; i <= 10000; i++) {
-        if(1 <= i && i < 100){
-            a =  i / 10;
-            b = i % 10;
-            dn[i+a+b] = 1;
-        } else if(100 <= i && i < 1000){
-            a = i / 100;
-            b = i / 10 % 10;
-            c = i % 10;
-            dn[i+a+b+c] = 1;
-        } else if (1000 <= i && i < 9999) {
-            a = i / 1000;
-            b = i / 10 / 10 % 10;
-            c = i / 10 % 10;
-            d = i % 10;
-            dn[i+a+b+c+d] = 1;
-        }
+		result = dn(i);
+		if(result <= 10000){
+			arr[result] = 1;
+		}
     }
 
-    for(i = 1; i <= n; i++){
-        if(!dn[i]){
+    for(i = 1; i <= 10000; i++){
+        if(!arr[i]){
             printf("%d\n", i);
         }
     }
