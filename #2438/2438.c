4f83dac6f24d1dadb87832ec6346ca466027f5d8
seungrokoh <devdavidoh@gmail.com>
2018-11-05 23:12:45 +0900

    #2438 풀이완료

---
+++ #2438/2438.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <>
+int number, data[1000001];
+
+void quickSort(int *data, int start, int end){
+    if (start >= end) {
+        return;
+    }
+    int key = start;
+    int i = start + 1, j = end, temp;
+    while (i <= j) {
+        while (data[i] <= data[key]) {
+            i++;
+        }
+        while (data[j] >= data[key] && j > start) {
+            j--;
+        }
+        if (i > j) {
+            temp = data[j];
+            data[j] = data[key];
+            data[key] = temp;
+        } else {
+            temp = data[i];
+            data[i] = data[j];
+            data[j] = temp;
+        }
+    }
+
+    quickSort(data, start, j - 1);
+    quickSort(data, j + 1, end);
+}
+
+
+int main(){
+    scanf("%d", &number);
+    for (int i = 0; i < number; i++) {
+        scanf("%d", &data[i]);
+    }
+
+    quickSort(data, 0, number - 1);
+    for (int i = 0; i < number; i++) {
+        printf("%d\n", data[i]);
+    }
+
+    return 0;
+}
