6e521ecdf2151cfd335bd81462486c0b2fae2c2f
seungrokoh <devdavidoh@gmail.com>
2018-11-10 23:27:13 +0900

    #1475 풀이완료

--- #1475/1475.c
+++ #1475/1475.c
@@ -9,35 +9,29 @@ int main() {
 
     scanf("%d", &number);
 
-    if(number <= 10){
-        printf("1");
-        return 0;
-    }
     while(1){
+        
+        index = number % 10;
         if(number == 0){
             break;
         }
-        index = number % 10;
-        arr[index] += 1;
+        
+        if(index == 9){
+			arr[6] += 1;
+		} else {
+			arr[index] += 1;
+		}
         number /= 10;
     }
 
-    arr[6] += arr[9];
-    if (arr[6] % 2 == 0) {
-        arr[6] /= 2;
-    } else {
-        arr[6] = (arr[6] / 2) + 1;
-    }
+    arr[6] = arr[6] / 2 + arr[6] % 2;
 
-    for(index = 0; index < 10; index++){
+    for(index = 0; index < 9; index++){
         if(max < arr[index]){
             max = arr[index];
         }
     }
 
-    for(index = 0; index < 10; index++){
-        printf("%d ", arr[index]);
-    }
-    printf("\n%d", max);
+    printf("%d", max);
     return 0;
 }
