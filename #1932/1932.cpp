c7e550053d971bd147dd9e3649b67c0cf9bccf52
seungrokoh <devdavidoh@gmail.com>
2018-11-12 22:12:06 +0900

    #1932 풀이완료

--- #1932/1932.cpp
+++ #1932/1932.cpp
@@ -1,37 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int arr[500][500];
+int arr[501][501];
+int dp[501][501];
+
 int main(void){
-	int n;
-	int i, j, sum = 0;
-	
-	scanf("%d", &n);
-	
-	for(i = 0; i < n; i++){
-		for(j = 0; j <= i; j++){
-			scanf("%d", &arr[i][j]);
-		}
-	}
-	
-	for(i = 0; i < n; i++){
-		for(j = 0; j <= i; j++){
-			printf("%d ", arr[i][j]);
-		}
-		printf("\n");
-	}
-	
-	sum += arr[0][0];
-	j = 0;
-	for(i = 0; i < n; i++){
-		if(arr[i+1][j] > arr[i+1][j+1]){
-			sum += arr[i+1][j];
-		} else {
-			sum += arr[i+1][j+1];
-			j++;
-		}
-	}
-	
-	printf("%d", sum);
-	return 0;
+    int n;
+    int i, j, sum = 0;
+    int max;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++){
+        for(j = 0; j <= i; j++){
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    dp[0][0] = arr[0][0];
+    for(i = 0; i < n; i++){
+        for(j = 0; j <= i; j++){
+            if (dp[i + 1][j] < dp[i][j] + arr[i + 1][j]) {
+                dp[i + 1][j] = dp[i][j] + arr[i + 1][j];
+            }
+            if (dp[i + 1][j + 1] < dp[i][j] + arr[i + 1][j + 1]) {
+                dp[i + 1][j + 1] = dp[i][j] + arr[i + 1][j + 1];
+            }
+        }
+    }
+
+    max = 0;
+    for(i = 0; i < n; i++){
+        if(max < dp[n-1][i]){
+            max = dp[n-1][i];
+        }
+    }
+
+    printf("%d", max);
+    return 0;
 }
