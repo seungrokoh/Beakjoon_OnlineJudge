0ed2d601b6f4deb5d264a9bf4ded77632023d055
seungrokoh <devdavidoh@gmail.com>
2018-11-14 00:41:20 +0900

    #1152 gets와 fgets의 차이 주석처리

--- #1152/1152.c
+++ #1152/1152.c
@@ -8,8 +8,13 @@ int main(void){
 
     int count = 1;
     int i;
-    //fgets (str, 1000001, stdin);
-    gets(str);
+    fgets (str, 1000001, stdin);
+    //gets(str);
+
+    //fgets는 엔터까지 저장
+    //따라서 str[strlen(str) - 2] == ' '로 해야함
+    //gets는 엔터값을 저장하지 않음
+    //따라서 str[strlen(str) - 1] == ' '로 해야함
 
     for (i = 0; i < strlen(str); i++) {
         if (str[i] == ' ') {
@@ -20,7 +25,7 @@ int main(void){
     if (str[0] == ' ') {
         count--;
     }
-    if (str[strlen(str) - 1] == ' ') {
+    if (str[strlen(str) - 2] == ' ') {
         count--;
     }
 
