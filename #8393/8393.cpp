68e71187a825fa11574786bde635996507e4ec3f
seungrokoh <devdavidoh@gmail.com>
2019-01-22 19:38:32 +0900

    #8393번 풀이완료

---
+++ #8393/8393.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int sum = 0;
+    int n;
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    cout << sum;
+    return 0;
+}
