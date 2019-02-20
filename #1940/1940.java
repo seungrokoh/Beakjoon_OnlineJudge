49f77f5c48a0301c73a44b0a1677884ace11241e
seungrokoh <devdavidoh@gmail.com>
2019-02-20 21:39:45 +0900

    add : #1940번 (주몽) 풀이완료

---
+++ #1940/1940.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n, m, cnt = 0;
+        n = in.nextInt();
+        m = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == m) {
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}
+
