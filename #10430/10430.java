8bb71c926fc240917397f103ec4c33dfb9c51ec5
seungrokoh <devdavidoh@gmail.com>
2019-02-25 19:55:40 +0900

    add : #10430번 (나머지) 풀이완료

---
+++ #10430/10430.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+        int C = Integer.parseInt(input[2]);
+
+        System.out.println((A+B)%C);
+        System.out.println((A%C + B%C)%C);
+        System.out.println((A*B)%C);
+        System.out.println((A%C * B%C)%C);
+    }
+}
+
