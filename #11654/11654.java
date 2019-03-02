db266407a153912587b626681ac05d39cf81eacd
seungrokoh <devdavidoh@gmail.com>
2019-03-02 11:27:50 +0900

    add : #11654번 (아스키 코드) 풀이완료

---
+++ #11654/11654.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        char ascii = in.next().charAt(0);
+
+        System.out.println((int) ascii);
+    }
+}
