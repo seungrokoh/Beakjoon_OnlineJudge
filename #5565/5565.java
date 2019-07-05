54c5cc761fc9b509667a9528e27b6e91dbab2ac4
seungrokoh <devdavidoh@gmail.com>
2019-07-05 19:28:36 +0900

    add : #5565번 (영수증) 풀이 완료

---
+++ #5565/5565.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    public static void main(String[] args) throws IOException {
+        int sum = Integer.parseInt(br.readLine());
+        for (int i = 0; i < 9; i++) {
+            sum -= Integer.parseInt(br.readLine());
+        }
+        System.out.println(sum);
+    }
+
+}
