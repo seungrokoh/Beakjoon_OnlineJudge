761e3e3ed1eff5a44aa3b34f9817b3441e0286f2
seungrokoh <devdavidoh@gmail.com>
2019-07-05 19:26:47 +0900

    add : #9498번 (시험성적) 풀이 완료

---
+++ #9498/9498.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    public static void main(String[] args) throws IOException {
+        int score = Integer.parseInt(br.readLine());
+        solve(score);
+    }
+
+    private static void solve(int score) {
+        if (score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
